54e296589706033856b1b03e6a991dd919e49ab5
unknown <colinad70@gmail.com>
2015-02-08 14:51:25 -0800

    milestone 1-18
    did allot of  work and cant remember what i did in all of the milestones

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  require_once(__DIR__ . '/veiw/header.php');
+  require_once(__DIR__ . '/veiw/navigation.php');
+  require_once(__DIR__ . '/controller/create-bd.php');
+  require_once(__DIR__ . '/veiw/footer.php');
+?>
+
