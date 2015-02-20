2f59392e76f1508ab1d3809d4dd74e20cb2c45c2
unknown <colinad70@gmail.com>
2015-02-20 14:59:14 -0800

    video 42 because I can :3

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,12 +1,24 @@
-<?php
-require_once(__DIR__ . "/../model/config.php");
+    <?php
+    require_once(__DIR__ . "/../model/config.php");
 
-$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
-$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-echo $password;
+    $salt = "$5$" . "rounds = 5000$" . uniqid(mt_rand(), true) . "$";
 
-$salt = "$5$" . "rounds = 5000$" . uniqid(mt_rand(), true) . "$";
+    $hashedpassword = crypt($password, $salt);
 
-echo $salt;
+    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+            . "email = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedpassword',"
+            . "salt = '$salt'");
+    
+    if($query) {
+        echo "Successfully created user: $username";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
+            
