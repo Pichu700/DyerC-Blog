d8217ae53f41868d1dd8df3d89ea11169e54f733
unknown <colinad70@gmail.com>
2015-02-23 15:03:29 -0800

    milestone 43
    can now see posts

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
   require_once(__DIR__ . '/veiw/navigation.php');
   require_once(__DIR__ . '/controller/create-bd.php');
   require_once(__DIR__ . '/veiw/footer.php');
+  require_once(__DIR__ . '/controller/read-posts.php');
 ?>
 
