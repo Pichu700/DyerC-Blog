df440c0d68a62098b5faa4f4a831b942e1b8f865
unknown <colinad70@gmail.com>
2015-02-26 15:16:05 -0800

    more stuff
    I dont know why it didnt get commited but now it is

---
+++ login.php
@@ -0,0 +1,5 @@
+<?php
+  require_once(__DIR__ . '/veiw/header.php');
+  require_once(__DIR__ . '/veiw/login-form.php');
+  require_once(__DIR__ . '/veiw/footer.php');
+?>
---
+++ veiw/bootstrap.php
@@ -0,0 +1,6 @@
+
+     <div class="jumbotron">
+      <h1>Hello, world!</h1>
+      <p>...</p>
+    </div>
+<button type="button" href="<?php echo $path . "login.php"?>" class="btn btn-default navbar-btn">Sign in</button>
